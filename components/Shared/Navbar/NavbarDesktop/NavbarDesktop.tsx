3d1c52e82734d17d60ef18b2e2b9380ee03d95c1
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-11 14:18:05 -0600

    useScrollposition hook

--- components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
+++ components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { BellRing, Search } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -5,12 +6,16 @@ import { cn } from "@/lib/utils";
 import { Logo } from "@/components/Shared/Logo";
 import { itemsNavbar } from "@/data/itemsNavbar";
 import Link from "next/link";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 
 export function NavbarDesktop() {
+  const scrollPosition = useScrollPosition();
+
   return (
     <div
       className={cn(
         "fixed left-0 right-0 z-30 h-16 w-full transition-all duration-300",
+        scrollPosition < 20 ? "bg-black" : "bg-transparent",
       )}
     >
       <div className="mx-auto h-full px-[4%]">
