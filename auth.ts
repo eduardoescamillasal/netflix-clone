ef1c91f278514d3edfccf0a28d851baa79b26d7b
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-14 15:09:10 -0600

    user model schema

--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/prisma";
+import { db } from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   providers: [],
 });
