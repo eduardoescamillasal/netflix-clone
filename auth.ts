36de76ed6e70d9e1cf0355fc485576bdc37b105a
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-15 10:54:38 -0600

    auth session callbacks with jwt

--- auth.ts
+++ auth.ts
@@ -5,6 +5,19 @@ import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      //session.user.anene = token.anene;
+      return session;
+    },
+    async jwt({ token }) {
+      //token.anene = "añeñe";
+      return token;
+    },
+  },
   session: { strategy: "jwt" },
   ...authConfig,
 });
