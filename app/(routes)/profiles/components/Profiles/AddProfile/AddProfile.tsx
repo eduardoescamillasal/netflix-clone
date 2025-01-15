1cc3f42e9431af8116239fd5616c693740446f44
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-15 12:09:44 -0600

    add profile dialog

---
+++ app/(routes)/profiles/components/Profiles/AddProfile/AddProfile.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { PlusCircle } from "lucide-react";
+
+export function AddProfile() {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <div className="group hover:cursor-pointer">
+          <div className="flex h-[140px] w-[140px] flex-col items-center justify-center rounded-md group-hover:bg-slate-300">
+            <PlusCircle className="h-16 w-16 text-gray-500" />
+          </div>
+          <p className="mt-2 text-lg uppercase text-gray-500">Add Profile</p>
+        </div>
+      </DialogTrigger>
+      <DialogContent className="bg-black sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Profile</DialogTitle>
+          <DialogDescription>
+            Add several profiles to your account.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- app/(routes)/profiles/components/Profiles/Profiles.tsx
+++ app/(routes)/profiles/components/Profiles/Profiles.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ProfilesProps } from "./Profiles.types";
+import { AddProfile } from "./AddProfile";
 
 export function Profiles(props: ProfilesProps) {
   const { users } = props;
   console.log({ users });
-  return <div>Profiles</div>;
+  return (
+    <div>
+      <div className="flex gap-7">
+        <p>Profile Users...</p>
+        <AddProfile />
+      </div>
+
+      <div className="mt-16 flex items-center justify-center">
+        <Button
+          variant="outline"
+          size="lg"
+          className="border-gray-500 text-gray-500"
+          onClick={() => console.log("click")}
+        >
+          Manage Profiles
+        </Button>
+      </div>
+    </div>
+  );
 }
