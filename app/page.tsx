92f514dbcf699d9dac0c548d202428d1d7c4d8dc
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-16 10:15:13 -0600

    session info in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
-import { Navbar } from "@/components/Shared/Navbar";
-import { SliderVideo } from "@/app/(routes)/(home)/components/SliderVideo";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-export default function Home() {
+import { Navbar } from "@/components/Shared/Navbar";
+
+import { SliderVideo } from "@/app/(routes)/(home)/components/SliderVideo";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return redirect("/login");
+  }
+
+  const usersNetflix = await db.userNetflix.findMany({
+    where: {
+      userId: session.user.id,
+    },
+  });
+
+  const movies = await db.movie.findMany();
+  const trendingMovies = await db.popularMovie.findMany({
+    orderBy: {
+      ranking: "asc",
+    },
+  });
+  console.log(movies);
+  console.log(trendingMovies);
   return (
     <div className="relative bg-zinc-900">
-      <Navbar />
+      <Navbar users={usersNetflix} />
       <SliderVideo />
     </div>
   );
